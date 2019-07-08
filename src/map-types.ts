cbcc71a9c0de25ff7b9b95c017bdc455f451b0ad
Raiondesu <a.raiondesu@gmail.com>
2019-07-08 16:01:55 +0300

    Add [overload] map-types - 3rd overload for better type inference

--- src/map-types.ts
+++ src/map-types.ts
@@ -10,6 +10,14 @@ export function mapTypes<
 export function mapTypes<
   I extends object,
   O extends object
+>(): <
+  F extends TypeMap<I, any>,
+  RealO extends object = InferOutput<I, F, O>
+>(input: I, FieldMap: F) => RealO;
+
+export function mapTypes<
+  I extends object,
+  O extends object
 >(input: I, FieldMap: TypeMap<I, O>): O;
 
 export function mapTypes<
