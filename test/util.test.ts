c804f762b29371d41f8ad077c7610c61712941d8
Raiondesu <a.raiondesu@gmail.com>
2019-07-08 17:00:56 +0300

    Add [*] test/util.test - all util tests

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,5 +1,49 @@
-describe('mapFactory', () => {
-  test('it tests', () => {
-    expect(true).toBe(true);
-  });
+import { isFlag, isConverter, isPropKey, isPropMapper, typedKeyOf } from '../src/util';
+
+test('isFlag', () => {
+  const flag = true;
+  const nonflag = 'true';
+
+  expect(isFlag(flag)).toBe(true);
+  expect(isFlag(nonflag)).toBe(false);
+});
+
+test('isConverter', () => {
+  const converter = (a) => a;
+  const nonconverter = '(a) => a';
+
+  expect(isConverter(converter)).toBe(true);
+  expect(isConverter(nonconverter)).toBe(false);
+});
+
+test('isPropKey', () => {
+  const propKey = 'key';
+  const nonpropKey = true;
+
+  expect(isPropKey(propKey)).toBe(true);
+  expect(isPropKey(nonpropKey)).toBe(false);
+});
+
+test('isPropMapper', () => {
+  const propMapper = { foo: String };
+  const nonpropMapper = true;
+  const nonpropMapper2 = { foo: true };
+
+  expect(isPropMapper<any, any>(propMapper, 'foo')).toBe(true);
+  expect(isPropMapper<any, any>(nonpropMapper, 'foo')).toBe(false);
+  expect(isPropMapper<any, any>(nonpropMapper2, 'foo')).toBe(false);
+});
+
+test('typedKeyOf', () => {
+  const obj = {
+    a: 0,
+    b: 1,
+    c: 2
+  };
+
+  expect(typedKeyOf(obj)).toMatchObject(['a', 'b', 'c']);
+
+  const key: Array<keyof typeof obj> = typedKeyOf(obj);
+
+  expect(Array.isArray(key)).toBe(true);
 });
