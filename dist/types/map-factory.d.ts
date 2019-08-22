d99fa3c10d9ea52eb8e2a49fb04c7fd246cc00d0
Raiondesu <a.raiondesu@gmail.com>
2019-08-22 18:36:03 +0300

    Update dist

--- dist/types/map-factory.d.ts
+++ dist/types/map-factory.d.ts
@@ -4,7 +4,7 @@ export declare function mapFactory<I extends object>(): <F extends TypeMap<I>, O
 export declare function mapFactory<I extends object, O extends object>(): <F extends TypeMap<I, O>, RealO extends object = InferOutput<I, F, O>>(fieldMap: F) => Converter<I, RealO>;
 export declare type InferConverter<I extends object, T extends TypeMap<I, O>, O extends object = InferOutput<I, T>> = Converter<I, O>;
 export declare type TypeMap<I extends object = any, O extends object = any> = {
-    [key in keyof O]: boolean | keyof I | Converter<I, O[key]> | PropertyMapper<I, O, key, Extract<key, keyof I>>;
+    [key in keyof O]: boolean | keyof I | Converter<I, O[key]> | PropertyMapper<I, O, key, Extract<key, keyof I>> | [Converter<Extract<key, keyof I>, O[key]>];
 };
 export declare type InferOutput<I extends object, T extends TypeMap<I, O>, O extends object = any> = {
     [key in keyof T]-?: T[key] extends true ? I[Extract<key, keyof I>] : T[key] extends keyof I ? I[T[key]] : T[key] extends Converter<I, infer OT> ? OT : T[key] extends Partial<Record<string, infer OT>> ? OT extends Converter<any, infer OKT> ? OKT : never : never;
